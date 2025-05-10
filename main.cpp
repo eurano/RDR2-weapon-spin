aa87587dd992ec3b82dbc407b12930819677c098
eurano <106998176+eurano@users.noreply.github.com>
2025-05-10 12:38:18 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include "main.h"
+#include "script.h"
+
+BOOL APIENTRY DllMain(HMODULE hInstance, DWORD reason, LPVOID lpReserved)
+{
+	switch (reason)
+	{
+	case DLL_PROCESS_ATTACH:
+		scriptRegister(hInstance, ScriptMain);
+		break;
+	case DLL_PROCESS_DETACH:
+		scriptUnregister(hInstance);
+		break;
+	}
+	return TRUE;
+}
+
+
---
+++ script.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "natives.h"
+#include "types.h"
+#include "enums.h"
+
+#include "main.h"
+
+#include "nativeCaller.h"
+
+void ScriptMain();
