cfcf81f0f498d5b4138735693c4d8372cc68821a
eurano <106998176+eurano@users.noreply.github.com>
2025-05-28 01:21:46 +0200

    Update script.cpp
    Performance optimization

--- script.cpp
+++ script.cpp
@@ -1,83 +1,66 @@
 #include "script.h"
-#include "natives.h"
-#include "types.h"
-#include "nativeCaller.h"
-#include "enums.h"
-#include "main.h"
 
-#if _MSC_VER > 1800  
 struct sGuid {
-	alignas(8) int data1;
-	alignas(8) int data2;
-	alignas(8) int data3;
-	alignas(8) int data4;
+    alignas(8) int data1;
+    alignas(8) int data2;
+    alignas(8) int data3;
+    alignas(8) int data4;
 };
-#else   //if Visual Studio 2013
-struct sGuid {
-	__declspec(align(8)) int data1;
-	__declspec(align(8)) int data2;
-	__declspec(align(8)) int data3;
-	__declspec(align(8)) int data4;
-};
-#endif
+
+void PlayGunSpinEmote(Ped ped, Hash emote, Hash spinType) {
+    WEAPON::_0xCBCFFF805F1B4596(ped, emote);
+    AI::_0xB31A277C1AC7B7FF(ped, 4, 1, emote, true, false, false, false);
+    WEAPON::_0x01F661BB9C71B465(ped, 1, spinType);
+    WEAPON::_SET_GUN_SPINNING_INVENTORY_SLOT_ID_ACTIVATE(ped, 0);
+}
 
 void update() {
     Ped playerPed = PLAYER::PLAYER_PED_ID();
 
-    const char* kit_emote_twirl_type = "KIT_EMOTE_TWIRL_GUN";
-    Hash emoteHash = GAMEPLAY::GET_HASH_KEY(const_cast<char*>(kit_emote_twirl_type));
-    const char* spin_type = "SHOULDER_TOSS";
-	Hash spin = GAMEPLAY::GET_HASH_KEY(const_cast<char*>(spin_type));
+    static DWORD lastSpinTime = 0;
+    static bool gunSpinInProgress = false;
+    const DWORD spinCooldown = 2000; 
 
-    const char* left_kit_emote_twirl_type = "KIT_EMOTE_TWIRL_GUN_LEFT_HOLSTER";
-    Hash leftEmoteHash = GAMEPLAY::GET_HASH_KEY(const_cast<char*>(left_kit_emote_twirl_type));
+    if (gunSpinInProgress && (GetTickCount() - lastSpinTime >= spinCooldown)) {
+        gunSpinInProgress = false;
+    }
 
-    const char* dual_kit_emote_twirl_type = "KIT_EMOTE_TWIRL_GUN_DUAL";
-    Hash dualEmoteHash = GAMEPLAY::GET_HASH_KEY(const_cast<char*>(dual_kit_emote_twirl_type));
+    Hash emoteHash = GAMEPLAY::GET_HASH_KEY(const_cast<char*>("KIT_EMOTE_TWIRL_GUN"));
+    Hash spinType = GAMEPLAY::GET_HASH_KEY(const_cast<char*>("SHOULDER_TOSS"));
+    Hash leftEmoteHash = GAMEPLAY::GET_HASH_KEY(const_cast<char*>("KIT_EMOTE_TWIRL_GUN_LEFT_HOLSTER"));
+    Hash dualEmoteHash = GAMEPLAY::GET_HASH_KEY(const_cast<char*>("KIT_EMOTE_TWIRL_GUN_DUAL"));
 
-    sGuid guidRight = {};
-    sGuid guidLeft = {};
+    if (PED::IS_PED_ON_MOUNT(playerPed)) return;
+    if (!AI::IS_PED_STILL(playerPed) || AI::IS_PED_SPRINTING(playerPed)) return;
+
+    Hash currentWeapon = 0;
+    if (!WEAPON::GET_CURRENT_PED_WEAPON(playerPed, &currentWeapon, true, 0, false)) return;
+    if (WEAPON::_0x0556E9D2ECF39D01(currentWeapon)) return;
+    if (!WEAPON::_0x705BE297EEBDB95D(currentWeapon)) return;
+
+    sGuid guidRight = {}, guidLeft = {};
+    int currentAmmoRight = 0, currentAmmoLeft = 0;
+
     bool gotRight = WEAPON::_0x6929E22158E52265(playerPed, 2, (Any*)&guidRight);
     bool gotLeft = WEAPON::_0x6929E22158E52265(playerPed, 3, (Any*)&guidLeft);
-
-    int currentAmmoRight = 0;
-    int currentAmmoLeft = 0;
     bool gotAmmoRight = WEAPON::_0x678F00858980F516(playerPed, (Any*)&currentAmmoRight, (Any*)&guidRight);
     bool gotAmmoLeft = WEAPON::_0x678F00858980F516(playerPed, (Any*)&currentAmmoLeft, (Any*)&guidLeft);
 
-    Hash currentWeapon = 0;
-    bool getCurrent = WEAPON::GET_CURRENT_PED_WEAPON(playerPed, &currentWeapon, true, 0, false);
-
-	bool dualWield = false;
-    if (currentAmmoRight == 0 && currentAmmoLeft == 0) {
-        dualWield = true;
-    }
-
-    bool isCurrentTwoHanded = WEAPON::_0x0556E9D2ECF39D01(currentWeapon);
-    bool isMounted = PED::IS_PED_ON_MOUNT(playerPed);
-    bool isStill = AI::IS_PED_STILL(playerPed);
-	bool isSprinting = AI::IS_PED_SPRINTING(playerPed);
-
-
-    if (CONTROLS::IS_CONTROL_JUST_PRESSED(0, 3350541322) && isStill && !isSprinting && currentWeapon != 0 && !isCurrentTwoHanded && !isMounted) {
-        if (currentAmmoRight == 0 && !dualWield) {
-            WEAPON::_0xCBCFFF805F1B4596(playerPed, emoteHash); //function needs to be added to natives.h manually
-            AI::_0xB31A277C1AC7B7FF(playerPed, 4, 1, emoteHash, true, false, false, false);
-            WEAPON::_0x01F661BB9C71B465(playerPed, 1, spin);
-            WEAPON::_SET_GUN_SPINNING_INVENTORY_SLOT_ID_ACTIVATE(playerPed, 0); //function needs to be added to natives.h manually
+    if (CONTROLS::IS_CONTROL_JUST_PRESSED(0, 3350541322) && !gunSpinInProgress) {
+        if (currentAmmoRight == 0 && currentAmmoLeft == 0) {
+            PlayGunSpinEmote(playerPed, dualEmoteHash, spinType);
+            gunSpinInProgress = true;
         }
-        else if (currentAmmoLeft == 0 && !dualWield) {
-            WEAPON::_0xCBCFFF805F1B4596(playerPed, leftEmoteHash); //function needs to be added to natives.h manually
-            AI::_0xB31A277C1AC7B7FF(playerPed, 4, 1, leftEmoteHash, true, false, false, false);
-            WEAPON::_0x01F661BB9C71B465(playerPed, 1, spin);
-            WEAPON::_SET_GUN_SPINNING_INVENTORY_SLOT_ID_ACTIVATE(playerPed, 0); //function needs to be added to natives.h manually
+        else if (currentAmmoRight == 0) {
+            PlayGunSpinEmote(playerPed, emoteHash, spinType);
+            gunSpinInProgress = true;
         }
-        else if (dualWield == true) {
-            WEAPON::_0xCBCFFF805F1B4596(playerPed, dualEmoteHash); //function needs to be added to natives.h manually
-            AI::_0xB31A277C1AC7B7FF(playerPed, 4, 1, dualEmoteHash, true, false, false, false);
-            WEAPON::_0x01F661BB9C71B465(playerPed, 1, spin);
-            WEAPON::_SET_GUN_SPINNING_INVENTORY_SLOT_ID_ACTIVATE(playerPed, 0); //function needs to be added to natives.h manually
+        else if (currentAmmoLeft == 0) {
+            PlayGunSpinEmote(playerPed, leftEmoteHash, spinType);
+            gunSpinInProgress = true;
         }
+
+        lastSpinTime = GetTickCount();
     }
 }
 
