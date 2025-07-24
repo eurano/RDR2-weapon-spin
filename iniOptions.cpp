abba8619b730020f7e8af130d3674e6bee26614d
eurano <106998176+eurano@users.noreply.github.com>
2025-07-24 19:06:09 +0200

    Update iniOptions.cpp
    1.5 
    - fixed BUG: rarely it was possible to spin two-handed weapon when such weapon was equiped as secondary weapon 
    - it is now possible to initiate gunspinning during walking but animation will start when player will stop 
    - it is now possible to stop gunspinning by pressing TAB (configurable in .ini file) 
    - gunspinning is now possible when mounted (configurable in .ini file, disabled by default)

--- iniOptions.cpp
+++ iniOptions.cpp
@@ -2,7 +2,9 @@
 
 IniOptions::IniOptions()
 {
-	this->keybind = 0x54;
+	this->spinKeybind = 0x54;
+	this->holsterKeybind = 0x09;
+	this->allowMounted = false;
 	this->loadOptions();
 }
 
@@ -10,9 +12,10 @@ int IniOptions::getIniValue(const char* value) {
 	if (value != nullptr) {
 		return std::atoi(value);
 	}
-	else {
-		return this->keybind;
-	}
+}
+
+bool IniOptions::getBoolIniValue(const char* value) {
+	return (value != nullptr && std::string(value) == "true");
 }
 
 IniOptions& IniOptions::loadOptions()
@@ -20,12 +23,24 @@ IniOptions& IniOptions::loadOptions()
 	SI_Error siError = this->generalIni.LoadFile("Spin.ini");
 	if (siError < 0) throw "Error!Could not find the ini file";
 
-	this->keybind = this->getIniValue(this->generalIni.GetValue("GENERAL", "keybind"));
+	this->spinKeybind = this->getIniValue(this->generalIni.GetValue("GENERAL", "spinKeybind", "0x54"));
+	this->holsterKeybind = this->getIniValue(this->generalIni.GetValue("GENERAL", "holsterKeybind", "0x09"));
+	this->allowMounted = this->getBoolIniValue(this->generalIni.GetValue("GENERAL", "allowMounted", "false"));
 
 	return *this;
 }
 
-int IniOptions::getKeybind()
+int IniOptions::getSpinKeybind()
 {
-	return this->keybind;
+	return this->spinKeybind;
+}
+
+int IniOptions::getHolsterKeybind()
+{
+	return this->holsterKeybind;
+}
+
+bool IniOptions::getAllowMounted()
+{
+	return this->allowMounted;
 }
